2bd8c483df5beaa432f8b796d88f1027ac6587a4
MohammadJavad <mj.ranjbar.94@gmail.com>
2019-07-08 02:12:18 -0400

    Edit Form for Profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,15 +14,17 @@ class UserController extends Controller
 
     public function update_avatar(Request $request)
     {
-		if($request->hasFile('avatar')){
-			$avatar = $request->file('avatar');
-			$filename = time() . '.' . $avatar->getClientOriginalExtension();
-			Image::make($avatar)->resize(300,300)->save(public_path('/uploads/avatars/'.$filename));
+	    $user = Auth::user();
 
-			$user = Auth::user();
-			$user->avatar = $filename;
-			$user->save();
-		}
+	    if ($request->hasFile('avatar')) {
+		    $avatar = $request->file('avatar');
+		    $filename = time() . '.' . $avatar->getClientOriginalExtension();
+		    Image::make($avatar)->resize(300,300)->save(public_path('/uploads/avatars/'.$filename));
+		    $user->avatar = $filename;
+
+	    }
+	    $user->name   = $request->name;
+	    $user->save();
 	    return view('profile',array('user' => Auth::user()));
 
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,14 +7,31 @@
 			<div class="col-md-10 col-md-offset-1">
 				<img src="/uploads/avatars/{{$user->avatar}}" style="width: 150px; height: 150px; float: left; border-radius:50%; margin-right: 25px;">
 				<h2>{{$user->name}}'s Profile</h2>
-				<form enctype="multipart/form-data" action="/profile" method="post">
-					<label> Update Profile Image</label>
-					<input type="file" name="avatar">
-					<input type="hidden" name="_token" value="{{csrf_token()}}">
-					<input type="submit" class="pull-right btn btn-sm btn-primary">
-				</form>
+				{{--<form enctype="multipart/form-data" action="/profile" method="post">--}}
+					{{--<label> Update Profile Image</label>--}}
+					{{--<input type="file" name="avatar">--}}
+					{{--<input type="hidden" name="_token" value="{{csrf_token()}}">--}}
+					{{--<input type="submit" class="pull-right btn btn-sm btn-primary">--}}
+				{{--</form>--}}
+
+
 			</div>
 		</div>
+	</div>
+	<div class="container border w-50" align="right" dir="rtl   ">
+		<form enctype="multipart/form-data" action="/profile" method="post" role="form">
+			@csrf
+			<legend >فرم ویرایش</legend>
+
+			<div class="form-group" align="right" dir="rtl">
+				<label for="name">تغیر نام کاربری</label>
+				<input type="text" class="form-control" name="name" id="name" placeholder="نام کاربری ...">
+				<label for="avatar">  تغییر عکس</label>
+				<input type="file" class="custom-file" name="avatar" id="avatar" >
+				<button type="submit" class="btn btn-primary mt-3" dir="rtl">تایید</button>
+
+			</div>
+		</form>
 	</div>
 
 
