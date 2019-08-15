59bf41c458cdc6da3dbc6f4fbbd0214fac4a5410
MohammadJavad <mj.ranjbar.94@gmail.com>
2019-08-15 12:35:07 -0400

    add Carousel for home page and change list group

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,52 +3,48 @@
 @section('content')
 
 	<div class="container">
-		<div class="row justify-content-center">
-
-			<div class="col-md-6">
-				<div class="card border-dark">
-					<div class="card-header" align="right" dir="rtl"> دسته بندی</div>
-			<div align="right" dir="rtl" style="font-size: 20px;">
-				@foreach($lists  as $list)
-					<ul>
-						<li><a href="{{route('list-book',['group' => $list->id])}}">{{$list->name}}</a></li>
-					</ul>
-				@endforeach
-			</div>
-			<br><br><br>
+		<div class="row justify-content-center ">
+			<div class="col-md-7">
+				<div class="popover-header" align="center" dir="rtl" style="background-color: #5a6268;color: aliceblue"> دسته
+					بندی
+				</div>
+				<div class="list-group" align="center" dir="rtl" style="font-size: 20px;">
+					@foreach($lists  as $list)
+						<ul class="list-group-item">
+							<li class="list-group-item "><a data-toggle="tooltip" data-placement="right"
+							                                title="{{$list->description}}"
+							                                href="{{route('list-book',['group' => $list->id])}}">{{$list->name}}</a>
+							</li>
+						</ul>
+					@endforeach
 
 				</div>
+				{{--<button class="btn btn-primary col-md-12">اضافه کردن دسته</button>--}}
 			</div>
-			<div CLASS="col-md-3"><div class="card border-dark">info</div></div>
+			<div class="col-md-4">
+				<div class="popover-header" align="center" dir="rtl" style="background-color: #5a6268;color: aliceblue">اضافه کردن دسته</div>
+				<div  align="right" dir="rtl">
+					<form action="{{route('post-list')}}" method="post" role="form">
+						@csrf
+						<div class="form-group">
+							<label for="name">نام دسته</label>
+							<input type="text" class="form-control" name="name" id="name" value="{{old('name')}}">
+						</div>
+						<div class="form-group">
+							<label for="description">توضیحات دسته</label>
+							<input type="text" class="form-control" name="description" id="description"
+							       value="{{old('description')}}">
+						</div>
+						<button type="submit" class="btn btn-primary col-md-12">تایید</button>
+					</form>
+				</div>
+			</div>
 		</div>
 	</div>
 	<br><br>
 	<div class="container">
-		<div class="row justify-content-center" >
+		<div class="row justify-content-center">
 
-			<div class="col-md-9">
-					<div class="card border-dark">
-						<div class="card-header" align="right" dir="rtl">اضافه کردن دسته </div>
-
-
-						<div class="form-group bg-transparent" align="right" dir="rtl">
-							<form action="{{route('post-list')}}" method="post" role="form">
-								@csrf
-								<div class="form-group">
-									<label for="name">نام دسته</label>
-									<input type="text" class="form-control" name="name" id="name" value="{{old('name')}}">
-								</div>
-								<div class="form-group">
-									<label for="description">توضیحات دسته</label>
-									<input type="text" class="form-control" name="description" id="description" value="{{old('description')}}">
-								</div>
-								<button type="submit" class="btn btn-primary">تایید</button>
-							</form>
-						</div>
-
-					</div>
-
-			</div>
 		</div>
 	</div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,78 @@
 <!doctype html>
 <html lang=fa>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
 	<script src="{{ asset('js/app.js') }}" defer></script>
-    <title>Laravel</title>
-
+	<title>کافه بوک</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
 </head>
 <body>
-<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4" dir="rtl">
-    <a class="navbar-brand" href="#">کافه بوک</a>
-	<a class="navbar-brand" href="{{route('list-group')}}" > دسته بندی </a>
-	<a class="navbar-brand" href="{{route('Forum')}}" > انجمن </a>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4" dir="rtl">
+		<a class="navbar-brand" href="#">کافه کتاب</a>
+		<a class="navbar-brand" href="{{route('list-group')}}"> دسته بندی </a>
+		<a class="navbar-brand" href="{{route('Forum')}}"> انجمن </a>
 
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+		        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active d-flex">
+					@if(!(auth()->check()))
+						<a class="nav-link" href="{{route('login')}}">ورود <span class="sr-only">(current)</span></a>
+					@endif
 
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active d-flex">
-                @if(!(auth()->check()))
-                    <a class="nav-link" href="{{route('login')}}">ورود <span class="sr-only">(current)</span></a>
-                @endif
+					<div>
 
-                    <div>
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+							@csrf
+						</form>
+					</div>
+				</li>
 
+			</ul>
+		</div>
+	</nav>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-            </li>
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
 
-        </ul>
-    </div>
-</nav>
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active" >
+				<img src="/uploads/1.jpg" alt="Los Angeles" style="width:100%;height: 300px">
+			</div>
 
-<main role="main" class="container" >
-    <div class="jumbotron" dir="rtl" align="justify" >
-        <h1>لورم ایپسوم </h1>
-        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
-        <a class="btn btn-lg btn-primary" href="{{route('register')}}" role="button">ثبت نام</a>
-    </div>
-</main>
+			<div class="item">
+				<img src="/uploads/1.jpg" alt="Chicago" style="width:100%;height: 300px">
+			</div>
+
+			<div class="item">
+				<img src="/uploads/1.jpg" alt="New york" style="width:100%;height: 300px">
+			</div>
+		</div>
+
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
 
 </body>
 </html>
