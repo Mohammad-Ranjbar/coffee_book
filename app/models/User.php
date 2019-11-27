71ddbe1184c90fa8968bbbd6689be70dd3553490
Mohammad Ranjbar <mj.ranjbar.94@gmail.com>
2019-11-27 10:13:15 +0330

    create relation between user & message

--- app/models/User.php
+++ app/models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\models;
 
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,25 +10,26 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','avatar'
+        'name',
+        'email',
+        'password',
+        'avatar',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
-
     /**
      * The attributes that should be cast to native types.
      *
@@ -36,24 +38,30 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-	// Relations
 
-	public function threads()
-	{
-		return $this->hasMany(Thread::class)->latest();
-	}
+    // Relations
 
-	public function activity()
-	{
-		return $this->hasMany(Activity::class);
-	}
+    public function threads()
+    {
+        return $this->hasMany(Thread::class)->latest();
+    }
 
-public function books()
-{
-    return $this->hasMany(Book::class);
-}
-public function comments()
-{
-    return $this->hasMany('App\models\Comment');
-}
+    public function activity()
+    {
+        return $this->hasMany(Activity::class);
+    }
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\models\Comment');
+    }
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
